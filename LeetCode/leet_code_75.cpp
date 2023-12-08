a2df4319aadf4d193b4d8a5c40c2584a498f9fc5
SwedishHouse <swedish2015@yandex.ru>
2023-12-08 16:58:43 +0400

    1768 solved

--- LeetCode/leet_code_75.cpp
+++ LeetCode/leet_code_75.cpp
@@ -2,19 +2,18 @@
 
 string leet_code_75::mergeAlternately(string word1, string word2)
 {
-    //unsigned int max_size = word1.size() > word2.size() ? word1.size(): word2.size();
     string* biggest = word1.size() >= word2.size() ? &word1 : &word2;
     string* smallest = word1.size() < word2.size() ? &word1 : &word2;
     char* result = new char[word1.size() + word2.size() + 1];
 
     for (int i(0); i < smallest->size(); i++)
     {
-        result[i*2] = (*biggest)[i];
-        result[i*2+1] = (*smallest)[i];
+        result[i*2] = word1[i];
+        result[i*2+1] = word2[i];
     }
-    for (int i(smallest->size()); i < biggest->size(); i++)
+    for (int i(0); i < biggest->size() - smallest->size(); i++)
     {
-        result[i*2] = (*biggest)[i];
+        result[smallest->size()*2 + i] = (*biggest)[smallest->size() +i];
     }
     result[word1.size() + word2.size()] = '\0';
     string str_res;
